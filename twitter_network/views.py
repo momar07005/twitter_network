a2035110106a8e58d4811639ca16409fd5475d61
wariored <cheikh1005@gmail.com>
2018-10-27 22:03:19 +0000

    second commit

--- twitter_network/views.py
+++ twitter_network/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from .relational_cirle import analyse
+import os
+from django.conf import settings as conf_settings
 
 
 def index(request):
-    return render(request, 'twitter_network/index.html')
+    file = open(os.path.join(conf_settings.BASE_DIR, 'test_data.csv'))
+    ana = analyse(file, 400, 400)
+    return render(request, 'twitter_network/index.html', {'ana': ana})
